105787950bc915f686f5dbf08c31fffe670d378f
sora1 <gonzalezevans05@gmail.com>
2023-04-28 17:31:00 -0500

    cambios 3

--- inicio/index.php
+++ inicio/index.php
@@ -1,15 +1,21 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <link rel="stylesheet" href="/css/principal.css">
-    <title>Document</title>
+    <link rel="stylesheet" href="/css/principal.css">
+    <link rel="stylesheet" href="/css/menucss/menu.css">
+    <link rel="shortcut icon" href="/img/3_favICON-dark.svg" type="image/x-icon">
+    <title>Inicio</title>
 </head>
+
 <body>
-    <h2>
-        hol
-    </h2>
+    <?php
+    include("../menu/menuprincipal.html");
+    include("./estructura.html");
+    ?>
 </body>
+
 </html>
