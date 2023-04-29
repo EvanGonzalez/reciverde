135cf92e874c70f70cf8701506b090d9c4a52ead
EvanNoNo <50892803+EvanGonzalez@users.noreply.github.com>
2023-04-29 09:06:14 -0500

    super cambios

--- inicio/index.php
+++ inicio/index.php
@@ -6,8 +6,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/css/principal.css">
-    <link rel="stylesheet" href="/css/menucss/menu.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
+    
     <link rel="shortcut icon" href="/img/3_favICON-dark.svg" type="image/x-icon">
+    
     <title>Inicio</title>
 </head>
 
@@ -16,6 +18,9 @@
     include("../menu/menuprincipal.html");
     include("./estructura.html");
     ?>
+
+    <script src="/js/menu.js"></script>
 </body>
+
 
 </html>
