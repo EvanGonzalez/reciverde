9d0f137bfdc9ea1e83049b5e00b518e6452e2e3d
sora1 <gonzalezevans05@gmail.com>
2023-05-05 16:52:22 -0500

    super cambios de pagina, base para trabaja loading

--- inicio/index.php
+++ inicio/index.php
@@ -6,8 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/css/principal.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
-    
+    <link rel="stylesheet" href="/css/footer.css">
     <link rel="shortcut icon" href="/img/3_favICON-dark.svg" type="image/x-icon">
     
     <title>Inicio</title>
@@ -17,10 +16,12 @@
     <?php
     include("../menu/menuprincipal.html");
     include("./estructura.html");
+    include("../menu/footer.html");
     ?>
 
     <script src="/js/menu.js"></script>
+    <script serc="/js/cards.js"></script>
 </body>
 
 
-</html>
+</html>     
--- js/menu.js
+++ js/menu.js
@@ -1,5 +1,8 @@
-$(document).ready(function(){
-    $('#icon').click(function(){
-        $('ul').toggleClass('show');
-    });
- });
+// nav toggle - select button and links
+const navToggle = document.querySelector("#navToggle")
+const nav = document.querySelector("#nav-links")
+
+// add event listener
+navToggle.addEventListener("click", () => {
+    nav.classList.toggle('nav-open')
+})
